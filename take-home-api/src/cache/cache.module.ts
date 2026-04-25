44b712e94a233b91b3ae9cd40ac5dda5c662012a
Iago-Santos-Sousa <iago.santos.sousa@gmail.com>
2026-04-25 19:07:26 -0300

    feat: add Redis cache module with asynchronous configuration

---
+++ take-home-api/src/cache/cache.module.ts
@@ -0,0 +1,24 @@
+import { Module } from "@nestjs/common";
+import { CacheModule } from "@nestjs/cache-manager";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import KeyvRedis from "@keyv/redis";
+
+@Module({
+  imports: [
+    CacheModule.registerAsync({
+      isGlobal: true,
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const redisUrl =
+          configService.get<string>("REDIS_URL") ?? "redis://localhost:6379";
+        return {
+          stores: [new KeyvRedis(redisUrl)],
+          ttl: 5 * 60 * 1000, // 5 minutes in ms
+        };
+      },
+    }),
+  ],
+  exports: [CacheModule],
+})
+export class RedisCacheModule {}
